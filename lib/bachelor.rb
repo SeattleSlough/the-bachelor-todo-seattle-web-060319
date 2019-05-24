a457e858ea4d2b9df93450fc33b9a9d829be55a0
Michael Maggs <michaelmaggs@gmail.com>
2019-05-24 17:11:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,41 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data[season].each do |contestants|
+    contestants.each do |key, value|
+      if value = "Winnner"
+        winner = contestants["name"]
+        array = winner.split
+        return array[0]
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.each do |stat, detail|
+        if detail == occupation
+          return contestant["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = []
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.each do |stat, detail|
+        if detail == hometown
+          count.push(contestant["name"])
+        end
+      end
+    end
+  end
+  return count.length 
 end
 
 def get_occupation(data, hometown)
