37ca63c89f83af0981d532954430f1bed5685250
Michael Maggs <michaelmaggs@gmail.com>
2019-05-24 17:54:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,9 +53,8 @@ end
 def get_average_age_for_season(data, season)
   age_array = []
   data[season].each do |contestants|
-      binding.pry
         contestant.each do |stat, detail|
-        
+        binding.pry
           age_array.push(contestant["age"])
     end
   end
