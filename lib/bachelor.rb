f1534ec878e451690495bd6c8795eb964a16cc13
Michael Maggs <michaelmaggs@gmail.com>
2019-05-24 17:26:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,7 +39,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestans.each do |contestant|
+      contestanteach.do |stat, detail|
+        if detail == hometown
+          return contestant[:occupation]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
